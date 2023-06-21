488ab0e83604fb9d3696691335b53634f87380bd
yurigba810 <yurigba810@gmail.com>
2023-06-21 17:57:04 -0300

    2 atualizações.  Essa foi atualização deixou o meu projeto com uma cara mais profissional. Agora a minha página inicial se chama index.  O intuito dessa modificação foi apenas para seguir uma linguagem mais voltada para a programação. paginaInicial não estava me agradando muito.
     O próximo ponto é uma melhoria na minha página de cálculo de ocupação. Agora, além de calcular a ocupação do hotel também calcula quantos apartamentos disponíveis tem no hotel.
     Adicionei a tag “h2” no meu html  para poder exibir o resultado.
    Adicionei no meu arquivo js os códigos necessários para selecionar a minha nova tag “h2”, calcular a quantidade de apartamentos disponíveis e por fim adicionar o resultado no meu html.
     E por fim, dei uma pequena ajustada no meu código css para que o resultado ficasse com um visual bacana para melhorar a experiência do usuário.

--- Ocupacao/ocupacao.js
+++ Ocupacao/ocupacao.js
@@ -1,5 +1,6 @@
 const botaoCalcular = document.getElementById('calcularOcupacao')
 const ocupacaoH1 = document.getElementById('valorOcupacao')
+const aptDisponiveisH2 = document.getElementById('aptDisponiveis')
 
 function calcularOcupacao() {
 
@@ -14,9 +15,13 @@ function calcularOcupacao() {
     const totalHosp = document.getElementById('totalHospedados').value
     const totalApt = document.getElementById('totalDeApartamentos').value
 
-    const calculo = (totalHosp / totalApt) * 100
-    
-    ocupacaoH1.textContent = `Ocupação:${calculo.toFixed(2)}%`
+    const calculoOcupacao = (totalHosp / totalApt) * 100
+    const calculoAptDispo = totalApt - totalHosp
+
+    ocupacaoH1.textContent = `Ocupação:${calculoOcupacao.toFixed(2)}%`
+
+
+    aptDisponiveisH2.textContent = `Apartamentos Disponíveis: ${calculoAptDispo}`
 }
 
 document.addEventListener('keydown', function (e) {
@@ -26,3 +31,4 @@ document.addEventListener('keydown', function (e) {
 })
 
 botaoCalcular.addEventListener('click', calcularOcupacao)
+
